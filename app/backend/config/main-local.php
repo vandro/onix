ffd468f757bb8388529636558c1d4b4ffd84462f
afsolarte <devsolarte@gmail.com>
2015-12-28 22:38:48 -0500

    Composer ajustado y ajustes locales

--- app/backend/config/main-local.php
+++ app/backend/config/main-local.php
@@ -1,37 +1,36 @@
 <?php
+$config = [];
 
-if (!YII_ENV_TEST) {
+if (YII_DEBUG) {
     // configuration adjustments for 'dev' environment
     $config['bootstrap'][]      = 'debug';
     $config['modules']['debug'] = 'yii\debug\Module';
     $config['bootstrap'][]      = 'gii';
     $config['modules']['gii']   = [
-        'class' => 'yii\gii\Module',
+        'class'      => 'yii\gii\Module',
         'generators' => [
-            'crud' => [
-                'class' => 'yii\gii\generators\crud\Generator',
-                'templates' => [
+            'crud'  => [
+                'class'               => 'yii\gii\generators\crud\Generator',
+                'templates'           => [
                     'onix' => '@backend/modules/onix/gii/crud/default'
                 ],
-                'modelClass' => 'common\models',
-                'controllerClass' => 'backend\controllers\Controller',
-                'viewPath' => '@backend/views/',
+                'modelClass'          => 'common\models',
+                'controllerClass'     => 'backend\controllers\Controller',
+                'viewPath'            => '@backend/views/',
                 'baseControllerClass' => 'common\controllers\BackController',
-                'searchModelClass' => 'backend\models\Search',
-                'template' => 'onix',
-                'enableI18N' => true,
-                'messageCategory' => 'back'
+                'searchModelClass'    => 'backend\models\Search',
+                'template'            => 'onix',
+                'enableI18N'          => true,
+                'messageCategory'     => 'back'
             ],
             'model' => [
-                'class' => 'yii\gii\generators\model\Generator',
-                'ns' => 'common\models',
-                'enableI18N' => true,
+                'class'           => 'yii\gii\generators\model\Generator',
+                'ns'              => 'common\models',
+                'enableI18N'      => true,
                 'messageCategory' => 'back'
             ]
         ],
-        'layoutPath' => '@backend/views/layouts',
-        'newFileMode' => 644,
-        'newDirMode' => 755
+        'layoutPath' => '@backend/views/layouts'
     ];
 }
 
