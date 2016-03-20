c982703106021e521ebe6e554b6d48b128835930
Andres Solarte <devsolarte@gmail.com>
2016-03-19 19:31:03 -0500

    Configuracion local para ver el debugger en el backend

--- app/backend/config/main-local.php
+++ app/backend/config/main-local.php
@@ -1,37 +1,43 @@
 <?php
-$config = [];
+$config = [ ];
 
-if (YII_DEBUG) {
-    // configuration adjustments for 'dev' environment
-    $config['bootstrap'][]      = 'debug';
-    $config['modules']['debug'] = 'yii\debug\Module';
-    $config['bootstrap'][]      = 'gii';
-    $config['modules']['gii']   = [
-        'class'      => 'yii\gii\Module',
-        'generators' => [
-            'crud'  => [
-                'class'               => 'yii\gii\generators\crud\Generator',
-                'templates'           => [
-                    'onix' => '@backend/modules/onix/gii/crud/default'
-                ],
-                'modelClass'          => 'common\models',
-                'controllerClass'     => 'backend\controllers\Controller',
-                'viewPath'            => '@backend/views/',
-                'baseControllerClass' => 'common\controllers\BackController',
-                'searchModelClass'    => 'backend\models\Search',
-                'template'            => 'onix',
-                'enableI18N'          => true,
-                'messageCategory'     => 'back'
-            ],
-            'model' => [
-                'class'           => 'yii\gii\generators\model\Generator',
-                'ns'              => 'common\models',
-                'enableI18N'      => true,
-                'messageCategory' => 'back'
-            ]
-        ],
-        'layoutPath' => '@backend/views/layouts'
-    ];
+if ( YII_DEBUG ) {
+	// configuration adjustments for 'dev' environment
+	$config['bootstrap'][]      = 'debug';
+	$config['modules']['debug'] = 'yii\debug\Module';
+	$config['bootstrap'][]      = 'gii';
+	$config['modules']['gii']   = [
+		'class'      => 'yii\gii\Module',
+		'generators' => [
+			'crud'  => [
+				'class'               => 'yii\gii\generators\crud\Generator',
+				'templates'           => [
+					'onix' => '@backend/modules/onix/gii/crud/default'
+				],
+				'modelClass'          => 'common\models',
+				'controllerClass'     => 'backend\controllers\Controller',
+				'viewPath'            => '@backend/views/',
+				'baseControllerClass' => 'common\controllers\BackController',
+				'searchModelClass'    => 'backend\models\Search',
+				'template'            => 'onix',
+				'enableI18N'          => true,
+				'messageCategory'     => 'back'
+			],
+			'model' => [
+				'class'           => 'yii\gii\generators\model\Generator',
+				'ns'              => 'common\models',
+				'enableI18N'      => true,
+				'messageCategory' => 'back'
+			]
+		],
+		'layoutPath' => '@backend/views/layouts',
+	];
+	$config['as access']        = [
+		'class'        => \backend\modules\onix\admin\components\AccessControl::className(),
+		'allowActions' => [
+			'debug/default/toolbar'
+		]
+	];
 }
 
 return $config;
