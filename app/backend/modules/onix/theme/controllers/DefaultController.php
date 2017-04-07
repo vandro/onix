6a5664f35d22e2e1e3543a4682ce1ee6bfc0a8a2
Andrés Felipe Solarte <devsolarte@gmail.com>
2017-04-06 22:23:33 -0500

    Avance modulo de administracion de temas

--- app/backend/modules/onix/theme/controllers/DefaultController.php
+++ app/backend/modules/onix/theme/controllers/DefaultController.php
@@ -15,6 +15,10 @@ class DefaultController extends BackController
 {
 	public function actionIndex()
 	{
+		/**
+		 * Aun no muevo el código de la vista al controlador porque estoy definiendo los campos a enviar a la vista,
+		 * la idea es definirlos y crear una tabla para el módulo y de la misma manera su respectiva migración
+		 */
 		return $this->render('index');
 	}
 }
